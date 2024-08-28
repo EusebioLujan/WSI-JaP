eb342bd8ec6ee7938e29049314c4d63f0afe3af6
Marcelo Lujan <115477257+EusebioLujan@users.noreply.github.com>
2024-08-28 20:24:47 -0300

    feat:(add new logic in login and logic navbar)

--- js/login.js
+++ js/login.js
@@ -2,12 +2,36 @@ document.getElementById("ingresar").addEventListener("click", function(e){
     e.preventDefault()
     const user = document.getElementById("username").value.trim()
     const password = document.getElementById("password").value.trim()
+    const usernameError = document.getElementById("usernameError");
+    const passwordError = document.getElementById("passwordError");
+    usernameError.textContent = "";
+    passwordError.textContent = "";
+    const usernameRegex = /^[a-zA-Z0-9_]+$/;
+    let valid = true;
 
-    if(!user || !password){
-        alert ("Campos vacíos")
-    }else {
-        localStorage.setItem("logeado",true)
-        localStorage.setItem("user", user)
-        window.location.href = "index.html"
+    if (!user) {
+        usernameError.textContent = "El nombre de usuario es obligatorio.";
+        valid = false;
     }
+    else if (!password) {
+        passwordError.textContent = "La contraseña es obligatoria.";
+        valid = false;
+    }
+    else if (user && !usernameRegex.test(user)) {
+        usernameError.textContent = "El nombre de usuario no debe contener signos especiales.";
+        valid = false;
+    }
+    else if (password && password.length < 6) {
+        passwordError.textContent = "La contraseña debe tener al menos 6 caracteres.";
+        valid = false;
+    }
+    else if (!user || !password){
+        alert("Los Campos no deben venir vacios")
+    }
+    else if (valid) {
+        localStorage.setItem("logeado", true);
+        localStorage.setItem("user", user);
+        window.location= "index.html";
+    }
+    
 })
