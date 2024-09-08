b835c37b3eebaf2cfc8d016ccac010357b900aae
Marcelo Lujan <115477257+EusebioLujan@users.noreply.github.com>
2024-09-08 19:42:39 -0300

    add info

--- js/product-info.js
+++ js/product-info.js
@@ -11,14 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+//se setea en el localstorage las id para poder trabajarlo en los productos relacionados
 function setrelatedproductID(id){
     localStorage.setItem("prodINFOID",id);
     window.location = "product-info.html"
 }
 
 function createProdInfo(Obj) {
+    //se seleciona con el slice las imagenes que esten en la pisicion 1 y 4 pero el slice el ultimo numero no te lo incluye entonces serian 3 imagenes
     let miniaturas = Obj.images.slice(1, 4); 
-
+    //aqui estoy mapeando los productos relacionados y utilizando el join para unirlos en una sola cadena para insertarlo en el html
     const relatedProducts = Obj.relatedProducts.map(product => `
         <div class="car" id="${product.id}">
             <h3>${product.name}</h3>
@@ -28,26 +30,22 @@ function createProdInfo(Obj) {
             </div>
         </div>
     `).join('');
-
-    const imagesHTML = miniaturas.map((image, index) => `
+    // aqui se esta mapeando miniaturas y guardandola en una variable
+    const imagesHTML = miniaturas.map((image) => `
         <img src="${image}" alt="Miniatura" class="img-event" data-index="${index + 1}">
     `).join('');
-
+    //bloque de codigo que se inserta en el HTML
     container_info.innerHTML = `
         <h1>${Obj.category}</h1>
         <h2 class="car-div-main">${Obj.name}</h2>
         <div class="car-main">
-
             <div class="car-details">
                 <div class="main-image">
                     <img class="car-image" id="main-image" src="${Obj.images[0]}" alt="${Obj.name}">
                 </div>
                 <p class="description">${Obj.description}</p>
-                
                 <p class="price">${Obj.currency} ${Obj.cost}</p>
-                
             </div>
-
             <div class="car-images">
                 ${imagesHTML}
                 <p class="sold-count">${Obj.soldCount} vendidos</p>
@@ -59,13 +57,18 @@ function createProdInfo(Obj) {
     `;
 
     let mainImage = document.getElementById('main-image');
+    //en esta variable se esta guardando la url de main image
     let mainImageSrc = mainImage.src;
 
     document.querySelectorAll(".car-images img").forEach((event) => {
         event.addEventListener("click", function () {
+            //se guarda en la variable la url del evento click es decir la url de la imagen
             let clickedImageSrc = event.src;
+            //se cambia la url main por la clickeada
             mainImage.src = clickedImageSrc;
+            //se cambia la url de la miniatura por la clickeada 
             event.src = mainImageSrc;
+            //aca se actualiza la variable con la url de la imagen clickeada
             mainImageSrc = clickedImageSrc;
         });
     });
